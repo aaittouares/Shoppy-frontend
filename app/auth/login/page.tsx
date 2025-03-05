7b7466ca83eb8bfb63c40fb3efb907af21f53b9c
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-03-06 00:16:31 +0100

    Add Login server action and modify login/page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,19 +1,56 @@
+'use client'
+
 import { Button, Link, Stack, TextField } from '@mui/material'
 import NextLink from 'next/link'
+import { useActionState, useState } from 'react'
+import login from './login'
 
 export default function Login() {
+  const [state, formAction] = useActionState(login, { error: '' })
+  //use controlled state for Textfields to keep inputs typed displayed when there is a submit error
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
   return (
-    <Stack spacing={2} className="w-full max-w-xs">
-      <TextField label="Email" variant="outlined" type="email"></TextField>
-      <TextField
-        label="Password"
-        variant="outlined"
-        type="password"
-      ></TextField>
-      <Button variant="contained">Login</Button>
-      <Link component={NextLink} href="/auth/signup" className="self-center">
-        Signup
-      </Link>
-    </Stack>
+    <form action={formAction} className="w-full max-w-xs">
+      <Stack spacing={2}>
+        <TextField
+          name="email"
+          label="Email"
+          variant="outlined"
+          type="email"
+          helperText={state.error}
+          error={!!state.error}
+          value={formData.email}
+          onChange={handleChange}
+        ></TextField>
+        <TextField
+          name="password"
+          label="Password"
+          variant="outlined"
+          type="password"
+          helperText={state.error}
+          error={!!state.error}
+          value={formData.password}
+          onChange={handleChange}
+        ></TextField>
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+        <Link component={NextLink} href="/auth/signup" className="self-center">
+          Signup
+        </Link>
+      </Stack>
+    </form>
   )
 }
