8bbbfc493964ab9cd2a2ce6ad1721bf0a89d6084
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-03-06 02:41:07 +0100

    Add middleware to protect non public routes

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from 'next/server'
+
+const publicRoutes = ['/auth/login', '/auth/signup']
+
+export function middleware(request: NextRequest) {
+  const auth = request.cookies.get('Authentication')?.value
+
+  if (
+    !auth &&
+    !publicRoutes.some((route) => request.nextUrl.pathname.startsWith(route))
+  )
+    return Response.redirect(new URL('/auth/login', request.url))
+}
+
+// Routes Middleware should not run on
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
