a6c6b5166553f369efbe4abcd01b74b906b8a299
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-03-20 14:46:04 +0100

    Add optionnal revalidation tags for next.js cache to the get function inside fetch util Mofify getProducts action to add a products tags to the function call Modify createProduct action to call revalidateTag on the products tag

--- app/products/actions/get-products.ts
+++ app/products/actions/get-products.ts
@@ -2,5 +2,5 @@ import { get } from '@/app/common/util/fetch'
 import { Product } from '../interfaces/product.interface'
 
 export default async function getProducts() {
-  return get<Product[]>('products')
+  return get<Product[]>('products', ['products'])
 }
