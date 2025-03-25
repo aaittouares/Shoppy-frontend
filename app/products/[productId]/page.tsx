8f2d9b1acc906d79f69eee912037c5156bb64225
Alexandre Aittouares <alex.aittouares@gmail.com>
2025-03-25 20:59:42 +0100

    Add get product server action Modify dynamic page for single product to display full product info Modify card component for products display to be clickable and redirect to single product page Modify process.env.API_URL to process.env.NEXT_PUBLIC_API_URL to be able to use it inside client components

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,7 +1,39 @@
+import { Stack, Typography } from '@mui/material'
+import getProduct from './get-product'
+import Image from 'next/image'
+import { getProductImage } from '../product-image'
+import Grid from '@mui/material/Grid2'
+
 interface SingleProductProps {
   params: { productId: string }
 }
 
 export default async function SingleProduct({ params }: SingleProductProps) {
-  return <p>Single Product {params.productId}</p>
+  //await necessary here for avoiding error message on server: see: https://nextjs.org/docs/messages/sync-dynamic-apis
+  const { productId } = await params
+  const product = await getProduct(+productId)
+
+  return (
+    <Grid container marginBottom={'2rem'} rowGap={3}>
+      {product.imageExists && (
+        <Grid size={{ md: 6, xs: 12 }}>
+          <Image
+            src={getProductImage(product.id)}
+            width={0}
+            height={0}
+            className="w-full sm:w-3/4 h-auto"
+            sizes="100vw"
+            alt="Picture of the product"
+          />
+        </Grid>
+      )}
+      <Grid size={{ md: 6, xs: 12 }}>
+        <Stack gap={3}>
+          <Typography variant="h2">{product.name}</Typography>
+          <Typography>{product.description}</Typography>
+          <Typography variant="h4">${product.price}</Typography>
+        </Stack>
+      </Grid>
+    </Grid>
+  )
 }
